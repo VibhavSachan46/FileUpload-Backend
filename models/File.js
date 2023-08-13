a02a3ab3f6bbc2c095340aacf475272d02ce9623
Vibhav <vibhavsachan743@gmail.com>
2023-08-13 12:53:01 +0530

    Backend completed

--- models/File.js
+++ models/File.js
@@ -1,20 +1,53 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    imageUrl:{
-        type:String,
+    imageUrl: {
+        type: String,
     },
-    tags:{
-        type:String,
+    tags: {
+        type: String,
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
     }
 });
+
+//post middleware
+fileSchema.post("save", async function(doc) {
+    try{
+        console.log("DOC",doc)
+
+        //transporter 
+        //TODO: shift this configuration under /config folder
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        //send mail 
+        let info = await transporter.sendMail({
+            from:`Vibhav`,
+            to: doc.email,
+            subject: "New File Uploaded on Cloudinary",
+            html:`<h2>Your file is successfully uploaded to Cloudinary</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+        })
+        
+        console.log("INFO", info);
+
+
+    }
+    catch(error) {
+        console.error(error);
+    }
+})
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
